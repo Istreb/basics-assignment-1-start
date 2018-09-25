44013bdfc27fb16dc5e0521cd92688393985d9ff
konstantin.kalichava <konstantin.kalichava@gotechsoftware.com>
2018-09-25 20:49:17 +0300

    initial commit

---
+++ src/app/SuccessAlert/SuccessAlert.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-successalert',
+  templateUrl: './SuccessAlert.component.html'
+})
+export class SuccessAlertComponent {
+
+}
